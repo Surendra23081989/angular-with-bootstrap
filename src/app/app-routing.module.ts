dbbdc698dfa8cfd8927a5233cc24e7e3afb2c6b2
Surendra23081989 <surendra.pratap23@gmail.com>
2024-05-14 10:03:17 +0530

    removed routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,39 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ButtonsComponent } from './buttons/buttons.component';
-import { CardsComponent } from './cards/cards.component';
-import { ColorsComponent } from './colors/colors.component';
-import { BordersComponent } from './borders/borders.component';
-import { AnimationsComponent } from './animations/animations.component';
-import { OthersComponent } from './others/others.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { ForgotComponent } from './forgot/forgot.component';
-import { NotFoundComponent } from './not-found/not-found.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { BlankPageComponent } from './blank-page/blank-page.component';
-import { TablesComponent } from './tables/tables.component';
-import { ChartsComponent } from './charts/charts.component';
 
-const routes: Routes = [
-  {path:'', component:DashboardComponent},
-  {path:'buttons', component:ButtonsComponent},
-  {path:'cards', component:CardsComponent},
-  {path:'colors', component:ColorsComponent},
-  {path:'borders', component:BordersComponent},
-  {path:'animations', component:AnimationsComponent},
-  {path:'others', component:OthersComponent},
-  {path:'login', component:LoginComponent},
-  {path:'register', component:RegisterComponent},
-  {path:'forgot-password', component:ForgotComponent},
-  {path:'blank-page', component:BlankPageComponent},
-  {path:'tables', component:TablesComponent},
-  {path:'charts', component:ChartsComponent},
-  {path:'**', component:NotFoundComponent},
-];
+const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],  
-exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
